105b2a86c8fe46b126f0e89c4476e324c9620210
Sara Öhman <saraohman97@gmail.com>
2024-05-25 14:13:17 +0200

    Prettier hover on projects.

--- app/(portfolio)/(components)/project-item.tsx
+++ app/(portfolio)/(components)/project-item.tsx
@@ -11,17 +11,19 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
       <a
         href={`https://${project.link}`}
         target="_blank"
-        className="relative group cursor-pointer"
+        className="relative group cursor-pointer rounded"
       >
         <Image
           src={project.images[0].url}
-          className="w-full group-hover:blur-sm shadow-xl max-h-[400px] object-cover"
+          className="w-full shadow-xl max-h-[400px] object-cover rounded"
           width={400}
           height={400}
           alt={project.title}
         />
-        <div className="invisible inset-0 flex items-center justify-center absolute group-hover:visible">
-          <Button>Gå till webbsidan</Button>
+
+        <div className="invisible inset-0 flex flex-col items-center justify-center gap-3 absolute group-hover:visible group-hover:bg-slate-900/80 text-white text-sm rounded">
+          <h3 className="text-xl font-bold">{project.title}</h3>
+          <p><span className="underline bg-blue-200 py-1 px-3 rounded text-black">Gå till webbsidan!</span></p>
         </div>
       </a>
   );
