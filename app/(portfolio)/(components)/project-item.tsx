42ce6aeaa1cbb06ed0958bcb54cf6bb6774df8ab
Sara Öhman <saraohman97@gmail.com>
2024-05-26 12:09:36 +0200

    Even prettier hover projects.

--- app/(portfolio)/(components)/project-item.tsx
+++ app/(portfolio)/(components)/project-item.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Project } from "@/types";
 import Image from "next/image";
 
@@ -8,24 +7,28 @@ interface ProjectItemProps {
 
 const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
   return (
-      <a
-        href={`https://${project.link}`}
-        target="_blank"
-        className="relative group cursor-pointer rounded"
-      >
-        <Image
-          src={project.images[0].url}
-          className="w-full shadow-xl max-h-[400px] object-cover rounded"
-          width={400}
-          height={400}
-          alt={project.title}
-        />
+    <a
+      href={`https://${project.link}`}
+      target="_blank"
+      className="relative group cursor-pointer rounded"
+    >
+      <Image
+        src={project.images[0].url}
+        className="w-full shadow-xl max-h-[400px] object-cover rounded"
+        width={400}
+        height={400}
+        alt={project.title}
+      />
 
-        <div className="invisible inset-0 flex flex-col items-center justify-center gap-3 absolute group-hover:visible group-hover:bg-slate-900/80 text-white text-sm rounded">
-          <h3 className="text-xl font-bold">{project.title}</h3>
-          <p><span className="underline bg-blue-200 py-1 px-3 rounded text-black">Gå till webbsidan!</span></p>
-        </div>
-      </a>
+      <div className="invisible -inset-6 flex flex-col items-center justify-center gap-3 absolute group-hover:visible group-hover:bg-slate-900/80 text-white text-sm rounded">
+        <h3 className="text-xl font-bold">{project.title}</h3>
+        <p>
+          <span className="underline bg-blue-200 py-1 px-3 rounded text-black">
+            Gå till webbsidan!
+          </span>
+        </p>
+      </div>
+    </a>
   );
 };
 
